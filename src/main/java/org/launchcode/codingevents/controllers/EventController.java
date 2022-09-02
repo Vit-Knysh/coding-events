1c707331942b0853006b219b1a700988d11fce67
Vit-Knysh <v.knysh85@gmail.com>
2022-09-01 20:41:12 -0400

    Display Validation Errors for a Field

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -26,6 +26,7 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -34,7 +35,7 @@ public class EventController {
                                          Errors errors, Model model) {
         if(errors.hasErrors()) {
             model.addAttribute("title", "Create Event");
-            model.addAttribute("errorMsg", "Invalid data!");
+//            model.addAttribute("errorMsg", "Invalid data!");
             return "events/create";
         }
         EventData.add(newEvent);
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -13,13 +13,13 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
-    @NotBlank
+    @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
     @Size(max = 500, message = "Description is too long!")
     private String description;
 
-    @NotBlank
+    @NotBlank(message = "Email is required")
     @Email(message = "Invalid email.Please, try again.")
     private String contactEmail;
 
@@ -29,6 +29,10 @@ public class Event {
         this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
+    }
+
+    public Event() {
+
     }
 
     public String getName() {
